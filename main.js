16156180443e7e61642a0a89ccaa786857facb00
rafajlpz <frilo_94@hotmail.com>
2021-12-26 19:24:27 +0100

    Creacion Vue App
    -Link vue3, js y html
    -Creacion Vue.createApp dentro con product y description

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: "Socks"
+            product: "Socks",
+            description: "Un par de calcetinos to wuapos",
         }
     }
 })
