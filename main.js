1c77943929af9675c8978ef7cf6f60f6b48f3499
rafajlpz <frilo_94@hotmail.com>
2021-12-30 21:59:16 +0100

    List Rendering
    -Uso de v-for= detail in details {{detail}
    -variant de variants {{variant}}
    -Uso de :key para seleccionar del DOM un elemento unico

--- main.js
+++ main.js
@@ -6,7 +6,12 @@ const app = Vue.createApp({
             image: "./assets/images/socks_blue.jpg",
             url: "https://lavozdegalicia.es",
             inStock: false,
-            inventory: 0
+            inventory: 0,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'},
+            ]
         }
     }
 })
