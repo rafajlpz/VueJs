110e0ebd015cc155f29b0c7a428d18f632e3c476
rafajlpz <frilo_94@hotmail.com>
2022-01-01 19:25:09 +0100

    Event Handling
    -Creacion de eventos y su uso como v-on:click
    -Escuchamos el evento @mouseover=
    -Creacion de methods, dentro sse creo addToCart() y udateImage() con sus .this

--- main.js
+++ main.js
@@ -9,9 +9,18 @@ const app = Vue.createApp({
             inventory: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green'},
-                {id: 2235, color: 'blue'},
-            ]
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
+            ],
+            cart:0 // Numero inicial en el carrito
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart += 1 //Aumento del producto en el carrito a mas uno por cada click
+        },
+        updateImage(variantImage){
+            this.image = variantImage
         }
     }
 })
